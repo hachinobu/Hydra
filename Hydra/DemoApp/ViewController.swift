864db81d30ba6593be9c50758e2b07ef8ec8b094
hachinobu <hachinobu@gmail.com>
2017-03-07 23:05:23 +0900

    update Sample

--- Hydra/DemoApp/ViewController.swift
+++ Hydra/DemoApp/ViewController.swift
@@ -13,7 +13,76 @@ class ViewController: UIViewController {
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
+//        promiseReduce()
+        promiseRecover()
 	}
+    
+    private func promiseRecover() {
+        
+        Promise<String> { resolve, reject in
+            return reject(PromiseError.rejected)
+//            return resolve("hachinobu")
+        }.recover { (e) -> Promise<String> in
+            return Promise(resolved: "nishinobu")
+        }.then { (s) in
+            print(s)
+        }
+        
+    }
+    
+    private func promiseReduce() {
+        
+        reduce([1, 2, 3], "hachinobu") { (str, num) in
+            let s = "\(str)\(num)"
+            return Promise(resolved: s)
+        }.then { (str) in
+            print(str)
+        }
+        
+    }
+    
+    private func promiseAll() {
+        let a = Promise(resolved: "a")
+        let b = Promise(resolved: "b")
+        let c = Promise(resolved: "c")
+        all([a, b, c]).then { (strs) in
+            print(strs)
+        }
+//        all(a, b, c).then { (strs) in
+//            
+//        }
+    }
+    
+    private func promiseMap() {
+        let strings = ["a", "b", "c", "d"]
+        map(as: .parallel, strings) { (str) -> Promise<Int> in
+            print(str)
+            return Promise(resolved: 1)
+        }.then { (nums) in
+            print(nums)
+        }
+    }
+    
+    private func always() {
+        Promise<String> { success, reject in
+            reject(PromiseError.rejected)
+            }.always {
+                print("always")
+        }.catch { (e) -> (Void) in
+            print(e)
+        }
+    }
+    
+    private func retry() {
+        Promise<String> { resolve, reject in
+            reject(PromiseError.rejected)
+        }.retry()
+            .then { (n) in
+                print(n)
+            }.catch { (e) -> (Void) in
+            print(e)
+        }
+    }
 	
 	func asyncFunc1() -> Promise<Int> {
 		return Promise<Int> { (resolve, reject) in
